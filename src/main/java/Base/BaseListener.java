e1f9b13e7fce7d6d22dbadaece758badcba5e3e1
v-sergiy.chimbay <v-sergiy.chimbay@shutterfly.com>
2022-05-26 22:51:16 +0300

    Implemented BaseListener, start implementing BasePage

--- src/main/java/Base/BaseListener.java
+++ src/main/java/Base/BaseListener.java
@@ -1,4 +1,72 @@
 package Base;
 
-public class BaseListener {
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.Logs;
+import org.openqa.selenium.remote.Augmenter;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class BaseListener implements IInvokedMethodListener, ITestListener {
+    @Override
+    public void onTestFailure(ITestResult result){
+        Reporter.logFail("Test has failed");
+        Reporter.logFail(getJSConsoleError());
+    }
+
+    public String getJSConsoleError(){
+        try {
+            String errorHeader = "Last console erroe is: ";
+            WebDriver augmentedDriver = new Augmenter().augment(getWebDriver());
+            Logs logs = augmentedDriver.manage().logs();
+            LogEntries logEntries = logs.get(LogType.BROWSER);
+            List<LogEntry> listOfLogs = logEntries.getAll();
+            if(listOfLogs.size() > 0){
+                LogEntry lastEntry = listOfLogs.get(listOfLogs.size() - 1);
+                if(lastEntry.getMessage().toLowerCase().contains("error")){
+                   errorHeader = errorHeader + lastEntry.getMessage();
+                } else {
+                    errorHeader = errorHeader + lastEntry.getMessage();
+                }
+                return errorHeader;
+            }
+            return "No errors were found";
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return "No errors were found";
+    }
+
+    protected void makeScreenshot(String methodname, String testName, ITestResult testResult){
+        WebDriver augmentDriver = new Augmenter().augment(getWebDriver());
+        File srcFile = ((TakesScreenshot) augmentDriver).getScreenshotAs(OutputType.FILE);
+        DateFormat dateFormat = new SimpleDateFormat("d:MM:yyyy");
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Kyiv"));
+        String currentDate = dateFormat.format(calendar.getTime());
+        String timeIndentifier = new SimpleDateFormat("d:MM:yyyy").format(calendar.getTime());
+        String fileName = methodname + timeIndentifier + ".png";
+        String fileLocation = System.getProperty("user.dir")+File.separator + "target" + File.separator + "result"+
+              File.separator + testName + File.separator + currentDate + File.separator;
+        try {
+            FileUtils.copyFile(srcFile, new File(fileLocation + fileName));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/Base/BasePage.java
+++ src/main/java/Base/BasePage.java
@@ -1,4 +1,64 @@
 package Base;
 
+import Utils.Reporter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import sun.security.mscapi.CPublicKey;
+
+import java.time.Duration;
+import java.util.List;
+
 public class BasePage {
+
+    private WebDriver driver;
+    private WebDriverWait webDriverWaitLonger;
+    private WebDriverWait webDriverWaitShorter;
+    private FluentWait<WebDriver> fluentWait;
+
+
+    public BasePage(){
+        this.driver = BaseTest.getWebDriver();
+        webDriverWaitLonger = new WebDriverWait(driver, Duration.ofSeconds(12));
+        webDriverWaitShorter = new WebDriverWait(driver, Duration.ofSeconds(2));
+        fluentWait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(7)).pollingEvery(Duration.ofMillis(700))
+                .ignoring(NoSuchElementException.class);
+    }
+
+    public WebElement findwebElement(By element){
+        Reporter.log("Getting web element " + element);
+        return driver.findElement(element);
+    }
+
+    public List<WebElement> findWebElements(By element){
+        Reporter.log("Getting the list of web elements " + element);
+        return driver.findElements(element);
+    }
+
+    public void clickWebElement(By element){
+        Reporter.log("Clicking on the web element " + element);
+        findwebElement(element).click();
+    }
+
+    public void clickWebElementByIndex(By elements, int webElementIndex){
+        Reporter.log("Clicking webElement by index " + webElementIndex + " from the list of webElements");
+        driver.findElements(elements).get(webElementIndex -1).click();
+    }
+
+    public String getWebElementText(By element){
+        Reporter.log("Extracting webElement text ");
+        String text = findwebElement(element).getText();
+        Reporter.log("Element text is "+text);
+        return text;
+    }
+
+    public String getWebElementAttributeValue(By element, String attribute){
+        Reporter.log("Extracting the value from the webElement");
+        String givenValue = findwebElement(element).getAttribute(attribute);
+        Reporter.log("The "+attribute+" from "+element+" is "+givenValue);
+        return givenValue;
+    }
 }
